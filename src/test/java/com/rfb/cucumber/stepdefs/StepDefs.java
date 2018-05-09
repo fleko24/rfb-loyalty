662cbbb59ef766773f2b44daac6a0dcefe87079d
Hernán <fleko24@hotmail.com>
2018-05-09 11:04:53 +0100

    Starting a new exercise using Angular and Spring Framework

---
+++ src/test/java/com/rfb/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,18 @@
+package com.rfb.cucumber.stepdefs;
+
+import com.rfb.RfbloyaltyApp;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.ResultActions;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@WebAppConfiguration
+@SpringBootTest
+@ContextConfiguration(classes = RfbloyaltyApp.class)
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
